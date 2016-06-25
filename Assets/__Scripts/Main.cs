079d5a09692c0331099208eff8d334440c86480c
kjl012989 <lee.kevinj@outlook.com>
2016-06-24 20:09:58 -0400

    Proj Completed

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -1,54 +1,87 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic ;
+using System.Collections.Generic;
 
 public class Main : MonoBehaviour {
-	static public Main S ;
 
+	static public Main S;
+	static public Dictionary<WeaponType, WeaponDefinition> W_DEFS;
 
-	public GameObject[] prefabEnemies ;
-	public float enemySpawnPerSecond = 0.5f ;
-	public float enemySpawnPadding = 1.5f ;
+	public GameObject[] prefabEnemies;
+	public float enemySpawnPerSecond = .5f;
+	public float enemySpawnPadding = 1.5f;
+	public WeaponDefinition[] weaponDefinitions;
 
-	public bool _______________ ;
-	public float enemySpawnRate ;
+	public GameObject prefabPowerUp;
+	public WeaponType[] powerUpFrequency = new WeaponType[]{
+		WeaponType.blaster,
+		WeaponType.blaster,
+		WeaponType.spread,
+		WeaponType.shield
+	};
 
+	public bool __________;
 
+	public WeaponType[] activeWeaponTypes;
+	public float enemySpawnRate;
 
-	void Awake () {
-
+	void Awake(){
 		S = this;
-		//Set Utils.cambounds
 		Utils.SetCameraBounds (this.camera);
-		//0.5 enemies/second = enemySpawnRate of 2
 		enemySpawnRate = 1f / enemySpawnPerSecond;
-		//Invoke call spawnenemy() once after a 2 second delay
-		Invoke ("SpawnEnemy", enemySpawnRate);
+		Invoke ("SpawnEnemy",enemySpawnRate);
 
+		W_DEFS = new Dictionary<WeaponType, WeaponDefinition> ();
+		foreach(WeaponDefinition def in weaponDefinitions){
+			W_DEFS[def.type] = def;
+		}
+	}
+
+	static public WeaponDefinition GetWeaponDefinition(WeaponType wt){
+		if(W_DEFS.ContainsKey(wt)){
+			return (W_DEFS[wt]);
+		}
+		return (new WeaponDefinition());
 	}
 
 
 	void Start(){
-
-		Screen.SetResolution (630, 900, false);
-
-		GameObject scoreGO = GameObject.Find ("ScoreCounter");
-
+		activeWeaponTypes = new WeaponType[weaponDefinitions.Length];
+		for(int i = 0; i < weaponDefinitions.Length; i++){
+			activeWeaponTypes[i] = weaponDefinitions[i].type;
+		}
 	}
 
 	public void SpawnEnemy(){
-		//pick a random enemy prefab to instantiate
-		int ndx = Random.Range (0,prefabEnemies.Length);
-		GameObject go = Instantiate (prefabEnemies[ndx])as GameObject ;
-		//position the Enemy above the screen with a random x positions
+		int ndx = Random.Range (0, prefabEnemies.Length);
+		GameObject go = Instantiate (prefabEnemies[ndx]) as GameObject;
 		Vector3 pos = Vector3.zero;
 		float xMin = Utils.camBounds.min.x + enemySpawnPadding;
-		float xMax = Utils.camBounds.max.x - enemySpawnPadding;
+		float xMax = Utils.camBounds.max.x + enemySpawnPadding;
 		pos.x = Random.Range (xMin, xMax);
 		pos.y = Utils.camBounds.max.y + enemySpawnPadding;
 		go.transform.position = pos;
-		//call spawnEnemy() again in a couple of seconds
 		Invoke ("SpawnEnemy", enemySpawnRate);
 	}
 
+	public void DelayedRestart(float delay){
+		Invoke ("Restart", delay);
+	}
+
+	public void Restart(){
+		Application.LoadLevel ("__Scene_0");
+	}
+
+	public void ShipDestroyed(Enemy e){
+		if(Random.value <= e.powerUpDropChance){
+			int ndx = Random.Range(0, powerUpFrequency.Length);
+			WeaponType puType = powerUpFrequency[ndx];
+
+			GameObject go = Instantiate(prefabPowerUp) as GameObject;
+			PowerUp pu = go.GetComponent<PowerUp>();
+			pu.SetType (puType);
+
+			pu.transform.position = e.transform.position;
+		}
+	}
 }
--- Assets/__Scripts/Shield.cs
+++ Assets/__Scripts/Shield.cs
@@ -3,27 +3,20 @@ using System.Collections;
 
 public class Shield : MonoBehaviour {
 
-	public float rotationsPerSecond = 0.1f;
-	public bool ___________________________;
+	public float rotiationsPerSecond = .1f;
+	public bool _________;
 	public int levelShown = 0;
-
-
-	// Use this for initialization
-	void Start () {
-
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		int currlevel = Mathf.FloorToInt (Hero.S.shieldLevel);
-		if (levelShown != currlevel) {
-			levelShown = currlevel;
+		int currLevel = Mathf.FloorToInt (Hero.S.shieldLevel);
+
+		if(levelShown != currLevel){
+			levelShown = currLevel;
 			Material mat = this.renderer.material;
-			mat.mainTextureOffset = new Vector2(0.2f*levelShown, 0f);
+			mat.mainTextureOffset = new Vector2 (.2f * levelShown, 0);
 		}
-
-		float rZ = (rotationsPerSecond * Time.time * 360) % 360f;
+		float rZ = (rotiationsPerSecond * Time.time*360)%360f;
 		transform.rotation = Quaternion.Euler (0, 0, rZ);
-
 	}
 }
